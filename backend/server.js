a860c9b54e960162c87dd42aac13ec9829aee026
umesh1177 <hp1707697@gmail.com>
2026-03-07 11:01:59 +0530

    Configure monorepo for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const http = require('http');
+const path = require('path');
 const { initSocket } = require('./services/socketService');
 
 const app = express();
@@ -90,10 +91,18 @@ notificationScheduler();
 tripCompletionChecker();
 cron.schedule('0 */6 * * *', checkEndOfTripOpportunity);
 
-// Basic Route
-app.get('/', (req, res) => {
-    res.send('SmartTrip API is running...');
-});
+// Serve Frontend in Production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
+    });
+} else {
+    app.get('/', (req, res) => {
+        res.send('SmartTrip API is running...');
+    });
+}
 
 // Start Server
 server.listen(PORT, () => {
